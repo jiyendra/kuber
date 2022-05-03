ee9fe15ecf8034cd6b3d1f0e9a699f2e367ac10a
root <root@ip-172-31-46-175.ap-south-1.compute.internal>
2022-05-03 16:09:24 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#uvicorn main:app --reload
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+#templates = Jinja2Templates(directory="/code")
+templates = Jinja2Templates(directory="C:/Users/jiyen/Desktop/k8_pro/k8_project")
+
+
+@app.get("/")
+def form_post(request: Request):
+    return templates.TemplateResponse('form.html', context={'request': request})
